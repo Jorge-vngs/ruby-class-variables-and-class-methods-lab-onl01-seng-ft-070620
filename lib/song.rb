089659b9f41f2737abc070cbf9219cce22c7fdd8
Jorge Venegas <brangelina01@gmail.com>
2020-07-18 22:13:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,8 @@
 class Song
-
   attr_accessor :name, :artist, :genre
-
-  @@song_count = 0
-  @@genres = []
-  @@artists = []
-
+    @@song_count = 0
+    @@genres = []
+    @@artists = []
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
@@ -14,16 +11,12 @@ class Song
     @@artists << @artist
     @@song_count +=1
   end
-
   def self.count
     @@song_count
   end
-
   def self.genres
     @@genres.uniq
   end
-
   def self.artists
     @@artists.uniq
   end
-end 
